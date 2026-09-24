c9df4bbeca25a73d3e81d14120588fbdf35136c2
Aleksandr Ilinykh <aleksandr.ilinykh@isax.com>
2026-09-24 12:29:52 +0200

    Reduce the KeyField bug to defaultValue instead of value

--- src/lib/KeyField.tsx
+++ src/lib/KeyField.tsx
@@ -1,6 +1,5 @@
 import { TextField } from '@mui/material';
-import debounce from 'lodash/debounce';
-import { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react';
+import { ChangeEvent, useCallback } from 'react';
 
 export type KeyFieldProps = {
   value: string;
@@ -8,18 +7,12 @@ export type KeyFieldProps = {
 };
 
 export function KeyField({ value, onChange }: KeyFieldProps) {
-  const [text, setText] = useState(value);
-  const emitChange = useMemo(() => debounce(onChange, 300), [onChange]);
-
-  useEffect(() => () => emitChange.cancel(), [emitChange]);
-
   const handleChange = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
-      setText(event.target.value);
-      emitChange(event.target.value);
+      onChange(event.target.value);
     },
-    [emitChange],
+    [onChange],
   );
 
-  return <TextField label="Order key" value={text} onChange={handleChange} />;
+  return <TextField label="Order key" defaultValue={value} onChange={handleChange} />;
 }
